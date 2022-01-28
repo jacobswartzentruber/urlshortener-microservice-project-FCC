83ac14d095601f1fb98bb9c9a3b4d9cbf2080b19
jacob.swartzentruber <jsswart@gmail.com>
2022-01-28 10:28:17 -0600

    Added routing/logic to create short URLs
    Added routes and logic to validate long URLs, save to database and return errors or short URL if successful

--- server.js
+++ server.js
@@ -1,22 +1,58 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
 const app = express();
+const { Schema } = require('mongoose');
+var mongoose = require('mongoose');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: "false"}));
 
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI);
+
+//Database Schema and Models
+const linkSchema = new Schema({
+  original_url: String,
+  short_url: String
+});
+
+const Link = mongoose.model("Link", linkSchema);
+
+//Routing
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res, next) {
+  //Validate form URL string
+  dns.lookup(req.body.url, (err, address) => {
+    if(err) {
+      res.json({error: 'invalid url'});
+    }
+    else{
+      //Calculate short URL based on number of documents
+      Link.countDocuments({}, function(err, count){
+        if(err) return next(err);
+
+        let link = new Link({original_url: req.body.url, short_url: count});
+
+        //Save to DB and return a JSON object with original_url and short_url
+        link.save(function(err, data) {
+          if(err) return next(err);
+          res.json({original_url: req.body.url, short_url: count});
+        })
+      })
+    }
+  })
+  
 });
 
 app.listen(port, function() {
